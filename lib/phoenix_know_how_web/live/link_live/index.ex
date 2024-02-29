c7c5b0ed69bb8be4114fe70f2ae67077a53a0720
Mauricio Massayuki <mauricio.soares@sptech.school>
2024-02-29 10:33:39 -0300

    feat: added forms into links page

--- lib/phoenix_know_how_web/live/link_live/index.ex
+++ lib/phoenix_know_how_web/live/link_live/index.ex
@@ -5,10 +5,41 @@ defmodule PhoenixKnowHowWeb.LinkLive.Index do
 
   def mount(_params, _session, socket) do
     user_id = socket.assigns.current_user.id
+    changeset = Links.Link.changeset(%Links.Link{})
+
     socket =
       socket
       |> assign(:links, Links.list_links(user_id))
+      |> assign(
+        :form,
+        to_form(changeset)
+      )
 
     {:ok, socket}
+  end
+
+  def handle_event("submit", %{"link" => link_params}, socket) do
+    params =
+      link_params
+      |> Map.put("user_id", socket.assigns.current_user.id)
+
+    case Links.create_link(params) do
+      {:ok, link} ->
+        socket =
+          socket
+          |> assign(:links, [link | socket.assigns.links])
+
+        {:noreply, socket}
+
+      {:error, changeset} ->
+        socket =
+          socket
+          |> assign(
+            :form,
+            to_form(changeset)
+          )
+
+        {:noreply, socket}
+    end
   end
 end
